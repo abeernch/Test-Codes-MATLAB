bcc979a07e5bc35f1e8a32d39716b0f15d9b206b
Abeer Nasir Chaudhry <168552036+abeernch@users.noreply.github.com>
2024-05-07 10:30:06 +0500

    Remove early RCS frequency comparison experiment

--- test_createVariables_inLoop_221229.m
+++
@@ -1,4 +0,0 @@
-for k=1:5
-    temp_var = strcat( 'x',num2str(k) );
-    eval(sprintf('%s = %g',temp_var,k*2));
-end
--- test_waypointDistanceCalc_230117.m
+++
@@ -1,7 +0,0 @@
-% Calculate total distance from waypoints
-for i = 2:length(tx{1}.trajPos)
-    wp1 = tx{1}.trajPos(:,i-1).';
-    wp2 = tx{1}.trajPos(:,i).';
-    dist(i) = norm(wp2 - wp1);
-end
-cum_dist = sum(dist)
